75d5add12f8ae35e530eb64e779c8cb5ac718afa
Usuario <Usuario@CPD02-M07>
2022-10-04 12:10:13 -0300

    Adicionar arquivos de projeto.

---
+++ AppPrevisaoTempo/AppPrevisaoTempo/Services/DataService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AppPrevisaoTempo.Model;
+
+namespace AppPrevisaoTempo.Services
+{
+   class DataServise
+    {
+         public static async Task<Tempo> GetPrevisaoDoTempo(string cidade)
+        {
+            string appId = "152dbc4cacb1df9b5ca15a18989c2396";
+
+            string queryString = "http://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&units=metric" + "&appid=" + appId;
+        }
+    }
+}
